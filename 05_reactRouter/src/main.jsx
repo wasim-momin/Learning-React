523418aa02a9e437d088b484153249d7194f9afc
Wasim Momin <wasim.momin14@gmail.com>
2025-08-07 10:10:30 +0530

    wip for router project

---
+++ 05_reactRouter/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:'/'
+  }
+])
+
+
+createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router}/>
+)
