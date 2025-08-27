51d12992372bbd6586e38287f8480b1681a4f060
Wasim Momin <wasim.momin14@gmail.com>
2025-08-27 19:25:08 +0530

    made services and store

--- 12_MegaBlog/src/App.jsx
+++ 12_MegaBlog/src/App.jsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react"
+import {useDispatch} from "react-redux"
+import authService from "./services/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from "./components"
+
 
 function App() {
-console.log(import.meta.env.VITE_PROJECT_URL)
-  return (
-   <>Hello </>
-  )
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className="min-h-height flex flex-wrap content-between bg-gray-400 ">
+      <div className="w-full block">
+        <Header />
+        <main>
+          
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+  <>Loading</>
+)
 }
 
 export default App
