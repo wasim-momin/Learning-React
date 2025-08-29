3e92176858c0b18b9056b2c1509a474a56025184
Wasim Momin <wasim.momin14@gmail.com>
2025-08-29 12:50:01 +0530

    header changes

--- 12_MegaBlog/src/components/Header.jsx
+++ 12_MegaBlog/src/components/Header.jsx
@@ -1,17 +1,48 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
+
+  const navigate = useNavigate()
+
+  const navItems = [
+    {
+      name:'Home',
+      url:'/',
+      active: true,
+    },
+    {
+      name:'Posts',
+      url:'/posts',
+      active: true,
+    },
+    {
+      name:'Create',
+      url:'/create',
+      active: true,
+    },
+    {
+      name:'Login',
+      url:'/login',
+      active: true,
+    },
+    {
+      name:'SignUp',
+      url:'/signup',
+      active: true,
+    },
+  ]
+
   return (
     <header className="bg-gray-800 bg-[#588ccb] text-white shadow p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-xl font-bold">MyBlog</Link>
-        <nav className="flex gap-4">
-          <Link to="/" className="px-3 py-0.5 rounded-md bg-[#588ccb]" >Home</Link>
-          <Link to="/posts" className="px-3 py-0.5 rounded-md bg-[#588ccb]" >Posts</Link>
-          <Link to="/create" className="px-3 py-0.5 rounded-md bg-[#588ccb]" >Create</Link>
-          <Link to="/login" className="px-3 py-0.5 rounded-md bg-[#588ccb]" >Login</Link>
-          <Link to="/signup" className="px-3 py-0.5 rounded-md bg-[#588ccb]" >Signup</Link>
-        </nav>
+        <ul className="flex gap-4">
+          {
+            navItems.map((navitem)=>(
+              <li key={navitem.name} className="cursor-pointer px-3 py-0.5 rounded-md bg-[#588ccb]" onClick={()=>navigate(navitem.url)}>{navitem.name}</li>
+            ))
+          }
+        </ul>
       </div>
     </header>
   );
