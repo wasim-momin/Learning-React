e3d269e507335590fe4b5594eaaa57485b787984
Wasim Momin <wasim.momin14@gmail.com>
2025-08-29 14:32:24 +0530

    auth store and header

--- 12_MegaBlog/src/components/Header.jsx
+++ 12_MegaBlog/src/components/Header.jsx
@@ -1,47 +1,59 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
+  console.log(authStatus);
 
   const navItems = [
     {
-      name:'Home',
-      url:'/',
+      name: "Home",
+      url: "/",
       active: true,
     },
     {
-      name:'Posts',
-      url:'/posts',
-      active: true,
+      name: "Posts",
+      url: "/posts",
+      active: !authStatus,
     },
     {
-      name:'Create',
-      url:'/create',
-      active: true,
+      name: "Create",
+      url: "/create",
+      active: authStatus,
     },
     {
-      name:'Login',
-      url:'/login',
-      active: true,
+      name: "Login",
+      url: "/login",
+      active: !authStatus,
     },
     {
-      name:'SignUp',
-      url:'/signup',
-      active: true,
+      name: "SignUp",
+      url: "/signup",
+      active: !authStatus,
     },
-  ]
+  ];
 
   return (
     <header className="bg-gray-800 bg-[#588ccb] text-white shadow p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-xl font-bold">MyBlog</Link>
+        <Link to="/" className="text-xl font-bold">
+          MyBlog
+        </Link>
         <ul className="flex gap-4">
-          {
-            navItems.map((navitem)=>(
-              <li key={navitem.name} className="cursor-pointer px-3 py-0.5 rounded-md bg-[#588ccb]" onClick={()=>navigate(navitem.url)}>{navitem.name}</li>
-            ))
-          }
+          {navItems.map((navitem) =>
+            navitem.active ? (
+              <li
+                key={navitem.name}
+                className="cursor-pointer px-3 py-0.5 rounded-md bg-[#588ccb]"
+                onClick={() => navigate(navitem.url)}
+              >
+                {navitem.name}
+              </li>
+            ) : (
+              null
+            )
+          )}
         </ul>
       </div>
     </header>
