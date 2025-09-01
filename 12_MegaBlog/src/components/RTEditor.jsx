044c3214b048a89151c1089bc30a758136b03991
Wasim Momin <wasim.momin14@gmail.com>
2025-09-01 14:00:11 +0530

    replace with textarea

--- 12_MegaBlog/src/components/RTEditor.jsx
+++ 12_MegaBlog/src/components/RTEditor.jsx
@@ -1,7 +1,14 @@
 import { Editor } from "@tinymce/tinymce-react";
+
+import "tinymce/tinymce";
+import "tinymce/icons/default";
+import "tinymce/themes/silver";
+import "tinymce/models/dom";
+
 import { Controller } from "react-hook-form";
 
 export default function RTEditor({ name, control, label, defaultValue = "" }) {
+
   return (
     <div className="w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
@@ -9,42 +16,15 @@ export default function RTEditor({ name, control, label, defaultValue = "" }) {
       <Controller
         name={name || "content"}
         control={control}
-        render={({ field: { onChange } }) => (
-          <Editor
-            initialValue={defaultValue}
-            onEditorChange={onChange}
-            init={{
-              initialValue: defaultValue,
-              height: 500,
-              menubar: true,
-              plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-              ],
-              toolbar:
-                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-              content_style:
-                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-            }}
-          />
+        defaultValue={defaultValue || ""}
+        render={({ field: { onChange, value } }) => (
+          <textarea
+            rows="5"
+            placeholder="Write your post content..."
+            className="px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full disabled:cursor-not-allowed disabled:opacity-70"
+            onChange={onChange}
+            value={value}
+          ></textarea>
         )}
       />
     </div>
